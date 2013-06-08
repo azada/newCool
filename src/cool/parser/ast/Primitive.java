66e0fb206005cb1c72803a4a269ba7ab3c68777c
pedram <p.rooshenas@gmail.com>
2013-06-08 12:26:32 -0700

    add generate function

---
+++ src/cool/parser/ast/Primitive.java
@@ -0,0 +1,18 @@
+package cool.parser.ast;
+
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pedram
+ * Date: 6/8/13
+ * Time: 12:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Primitive extends ClassNode
+{
+    public Primitive(String type, ArrayList varFormals, Extends ext, ArrayList featureList) {
+        super(type, varFormals, ext, featureList);
+    }
+
+}
