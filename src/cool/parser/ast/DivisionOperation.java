fde619c00d79f2caae972a321df479c979e43575
azada <azada@cs.uoregon.edu>
2013-06-13 19:20:30 -0700

    push

--- src/cool/parser/ast/DivisionOperation.java
+++ src/cool/parser/ast/DivisionOperation.java
@@ -1,5 +1,9 @@
 package cool.parser.ast;
 
+import cool.codegen.ActivationRecord;
+import cool.codegen.ActivationStack;
+import cool.codegen.Binding;
+import cool.codegen.CodeGenerator;
 import cool.exception.MyException;
 import cool.symbol.SymbolNode;
 
@@ -53,5 +57,32 @@ public class DivisionOperation extends RealOperation {
 
         this.expType = INTEGER_TYPE;
         return result;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+    @Override
+    public void generate(StringBuilder builder) {
+        ActivationRecord record = ActivationStack.getHandle().top();
+        Expr op1 = (Expr)operandsList.get(0);
+        Expr op2 = (Expr)operandsList.get(1);
+        op1.generate(builder);
+        op2.generate(builder);
+        Binding op1Binding = CodeGenerator.loadExpr(builder, op1);
+        Binding op2Binding = CodeGenerator.loadExpr(builder,op2);
+        Binding resultBinding = record.bindToExpr(this);
+
+        ClassNode op1Node = Program.getClassNode(op1.expType);
+        CodeGenerator.allocatePointer(builder, resultBinding, op1Node);
+        int tempVar = record.getNewVariable();
+        builder.append("%" + tempVar + " = " + "sdiv " );
+
+
+        op1Node.generateInstance(builder);
+        builder.append(" %" + op1Binding.getLoadedId() + ", " + "%" + op2Binding.getLoadedId());
+        CodeGenerator.newLine(builder);
+
+        CodeGenerator.storeExpr(builder,tempVar,resultBinding);
+
+        CodeGenerator.newLine(builder);
+
+
     }
 }
